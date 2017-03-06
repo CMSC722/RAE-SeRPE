10ee06add5a7f8d2263d35a4d0146b4e3bcfc1f0
gkwatson <gkwatson@email.wm.edu>
2017-03-06 00:47:29 -0500

    Unification on task/method arguments with state
    The unification appears to be working with phony state and
    method-library representations given at the bottom of the file. Next we
    need to get this working with an actual representation.

--- src/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, 'C:\Users\Gavin\Documents\GitHub\RAE-SeRPE\resources\domains\test_domain1')
-sys.path.insert(0, 'C:\Users\Gavin\Documents\GitHub\RAE-SeRPE\src')
-
-import interpreter
-
-print_asts("test_domain1", debug=False)
