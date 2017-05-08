faf36c9c703a15bd8d13fec7e839618d724be89e
Matt Goldberg <goldburger@users.noreply.github.com>
2017-05-08 16:45:59 -0400

    SeRPE now works for basic refinement of method to action, without a recursive SeRPE call.

--- src/SeRPE.py
+++ src/SeRPE.py
@@ -1,29 +1,55 @@
+import copy
+from interpreter import *
 import planning_problem
+import RAE
 
 # None return used as failure representation (?)
 
 def SeRPE(refine_methods, action_templates, state, task):
-  candidates = instances(refine_methods, task, state)
+  print "Beginning SeRPE call..."
+  candidates = RAE.getCandidates(refine_methods, task, state, True)
+  print "Candidates were:\n"
+  print candidates
   if len(candidates) == 0:
     return None
   # nondeterministic choice currently as DFS
   for m in candidates:
+    state_copy = copy.deepcopy(state)
     result = progressToFinish(refine_methods, action_templates, state, task, m)
     if result != None:
       return result
+    state = state_copy
   return None
 
-def instances(refine_methods, task, state):
-  # likely identical to RAE's getCandidates method?
-  return []
-
 def progressToFinish(refine_methods, action_templates, state, task, m):
+  print "Progressing to finish..."
   plan = []
+  print "Instantiating interpreter"
+  interp = Interpreter(refine_methods[m[0]], m[1], state, pp.task_table, pp.commands)
+  for (node_type, node_id, node_args) in interp:
+    args = tuple()
+    for arg in node_args:
+      args = args + (arg['val'],)
+    if node_type == "ACTION":
+      print "Processing command" + str(node_id)
+      action = action_templates[node_id]
+      args = (state,) + args
+      succeeded = action(*args)
+      if succeeded:
+        print "Command succeeded"
+        plan.append(action)
+      else:
+        return None
+    elif node_type == "TASK":
+      print "Processing task" + str(node_id)
+      plan_prime = SeRPE(refine_methods, action_templates, state, (node_id, args))
+      if plan_prime != None:
+        plan.append(plan_prime)
+      else:
+        return None
+  return plan
 
-  # how much of logic in SeRPE pseudocode is handled by interpreter?
-  # TODO: fill in based on interpreter API
-
-planningProblem = planning_problem.PlanningProblem("./parsing/trivial_pp.zip");
-print(planningProblem)
-
-SeRPE([], [], None, None)
+pp = planning_problem.PlanningProblem("../domains/simple_domain.zip")
+print pp
+result = SeRPE(pp.method_table, pp.action_models, pp.domain, ('get-cargo', ('c1',)))
+print result
