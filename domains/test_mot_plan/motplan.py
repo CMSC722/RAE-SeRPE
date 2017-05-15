1eebce57c11157eb841ab1703c7a13449cee17c8
gkwatson <gkwatson@email.wm.edu>
2017-05-14 23:55:55 -0400

    Small Mot plan fix and more RAE prints for debugging

--- domains/test_mot_plan/motplan.py
+++ domains/test_mot_plan/motplan.py
@@ -7,7 +7,7 @@ from RAE import *
 arena=map.map()
 robo=robot.robot('A',arena)
 ppi = planning_problem.PlanningProblem('Archive.zip')
-rae=Rae(ppi.method_table, ppi.commands, ppi.domain, ppi.task_table, ('move-to-room', ('r1','A','H')))
+rae=Rae(ppi.method_table, ppi.commands, ppi.domain, ppi.task_table, ('move-to-room', ('r1','A','H')), debug_flag=True)
 
 # arena=map.map()
 # robo=robot.robot('A',arena)
